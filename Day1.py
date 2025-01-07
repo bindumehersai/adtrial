4947edef7669f05b446c3cc28f993a84cb20670f
T Bindu Meher Sai <bindumeher10@gmail.com>
2025-01-07 14:49:35 +0000

    update

---
+++ Day1.py
@@ -0,0 +1,12 @@
+"""Write a program in Jupyter Notebook to declare variables of different 
+data types (integer, float, string, and boolean). Print each variable and its type."""
+
+a = 10
+b = 3.14
+c = "Hello, World!"
+d = True
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
